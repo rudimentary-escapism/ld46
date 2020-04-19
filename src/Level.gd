092f1b4856c80d309fc16325f357fab1307a7170
alekseypirogov <iartifical@gmail.com>
2020-04-19 22:53:20 +0700

    added a zombie

--- src/Level.gd
+++ src/Level.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 func _on_Player_attacked():
-    $CanvasLayer/DeatchScreen.visible = true
+	$CanvasLayer/DeatchScreen.visible = true
 
 
 func _on_DeatchScreen_restart():
-    get_tree().reload_current_scene()
+	get_tree().reload_current_scene()
